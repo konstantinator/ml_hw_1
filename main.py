dc92491160947c22684a8e2f082bceeba97b05c4
konstantin <konstantin7500@yandex.ru>
2023-12-03 21:37:05 +0300

    main main 2

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ def predict_items(items: List[Item]) -> List[float]:
 
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
